01555aae08c99faf3f35918e9789f84408ae3ba5
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2016-09-18 22:31:16 +0900

    chore: Add task 'stylelint'

--- gulp/tasks/lint.ts
+++ gulp/tasks/lint.ts
@@ -3,3 +3,4 @@ import {execNodeTask} from '../task_helpers';
 
 
 gulp.task('tslint', execNodeTask('tslint', ['-c', 'tslint.json', 'src/**/*.ts']));
+gulp.task('stylelint', execNodeTask('stylelint', ['src/**/*.scss', '--config', '.stylelintrc.json', '--syntax', 'scss']));
